53d7469dd9b3fcb4689e04517090dfdf0c10ebfe
azhammakestrash <azhamproductions@proton.me>
2024-06-13 12:00:15 +0200

    Up : Updated /events

--- src/bot/events/messageDeleteBulk.js
+++ src/bot/events/messageDeleteBulk.js
@@ -1,71 +1,64 @@
-const sa = require('superagent')
-const getMessagesByIds = require('../../db/interfaces/postgres/read').getMessagesByIds
+const { getMessagesByIds } = require('../../db/interfaces/postgres/read').getMessagesByIds
 const send = require('../modules/webhooksender')
-const { EMBED_COLORS } = require('../utils/constants')
+const { displayUser } = require('../utils/constants')
+const { createHaste } = require('../utils/createHaste')
 
 module.exports = {
   name: 'messageDeleteBulk',
   type: 'on',
   handle: async messages => {
-    if (messages.length === 0) return // TODO: TEST!
-
-    if (!process.env.PASTE_CREATE_ENDPOINT) {
-      if (!messages[0].guildId) return
-
-      return send({
-        guildID: messages[0].guildId,
-        eventName: 'messageDeleteBulk',
-        embeds: [{
-          description: `${messages.length} messages were bulk deleted. :warning: The bot owner hasn't configured a paste site so contents of deleted messages not shown. :warning:`,
-          color: EMBED_COLORS.YELLOW_ORANGE
-        }]
-      })
-    }
+    if (!messages.length) return // TODO: TEST!
+    if (!messages[0].guildId) return;
 
     const dbMessages = await getMessagesByIds(messages.map(m => m.id))
-    await paste(dbMessages, messages[0].channel.guild.id)
+    await paste(dbMessages, messages[0].guildId)
   }
 }
 
-async function paste (messages, guildID) {
+async function paste(messages, guildID) {
   if (!messages) return
-  const messageDeleteBulkEvent = {
-    guildID,
-    eventName: 'messageDeleteBulk',
-    embeds: [{
-      description: `**${messages.length}** message(s) were deleted and known in cache.`,
-      fields: [],
-      color: 15550861
-    }]
-  }
+//  const messageDeleteBulkEvent = {
+//    guildID,
+//    eventName: 'messageDeleteBulk',
+//    embeds: [{
+//      description: `**${messages.length}** message(s) were deleted and known in cache.`,
+//      fields: [],
+//      color: 15550861
+//    }]
+//  }
   const pasteString = messages.reverse().map(m => {
     let globalUser = global.bot.users.get(m.author_id)
     if (!globalUser) {
       globalUser = {
         username: 'Unknown',
-        discriminator: '0000',
+        discriminator: '0',
         avatarURL: '<no avatar>'
       }
     }
-    return `${globalUser.username}#${globalUser.discriminator} (${m.author_id}) | (${globalUser.avatarURL}) | ${new Date(m.ts).toUTCString()}: ${m.content}`
+    return `${displayUser(globalUser)} (${m.author_id}) | (${globalUser.avatarURL}) | ${new Date(m.ts).toUTCString()}: ${m.content}`
   }).join('\r\n')
-  if (pasteString) {
-    sa
-      .post(process.env.PASTE_CREATE_ENDPOINT)
-      // .set('Authorization', process.env.PASTE_CREATE_TOKEN)
-      .set('Content-Type', 'text/plain')
-      .send(pasteString || 'An error has occurred while fetching pastes. Please contact the bot author.')
-      .end((err, res) => {
-        if (!err && res.body && res.statusCode === 200 && res.body.key) {
-          messageDeleteBulkEvent.embeds[0].fields.push({
-            name: 'Link',
-            value: `${process.env.PASTE_CREATE_ENDPOINT.endsWith("/") ? process.env.PASTE_CREATE_ENDPOINT.slice(0, -1) : process.env.PASTE_CREATE_ENDPOINT}/${res.body.key}`
-          })
-          send(messageDeleteBulkEvent)
-        } else {
-          global.logger.error(err)
-          global.webhook.error('An error has occurred while posting to the paste website. Check logs for more.')
-        }
-      })
+  if (!pasteString) {
+    return
   }
+  const messageDeleteBulkEvent = {
+    guildID: guildID,
+    eventName: 'messageDeleteBulk',
+    embeds: [{
+      description: `**${messages.length}** message(s) were deleted and known in cache.`,
+      fields: [],
+      color: 15550861
+    }],
+    file: [
+      // Send a messages.txt file to the channel.
+      { name: `messages.txt`, file: Buffer.from(pasteString) }
+    ]
+  }
+  const link = await createHaste(pasteString)
+  if (link) {
+    messageDeleteBulkEvent.embeds[0].fields.push({
+      name: 'Link',
+      value: link
+    })
+  }
+  send(messageDeleteBulkEvent)
 }
