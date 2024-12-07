08ed746fa1e4a37bbd39469baa6e1d35ae1e7967
Azham Makes Trash <73955243+AzhamProdLive@users.noreply.github.com>
2024-12-07 21:56:05 +0100

    Update guildRoleCreate.js

--- src/bot/events/guildRoleCreate.js
+++ src/bot/events/guildRoleCreate.js
@@ -1,5 +1,5 @@
 const send = require('../modules/webhooksender')
-const { displayUsername } = require('../utils/constants')
+const { displayUser } = require('../utils/constants')
 
 module.exports = {
   name: 'guildRoleCreate',
@@ -35,7 +35,7 @@ module.exports = {
         if (log.reason) guildRoleCreateEvent.embeds[0].fields[1].value = log.reason
         guildRoleCreateEvent.embeds[0].fields[2].value = `\`\`\`ini\nRole = ${role.id}\nPerpetrator = ${perp.id}\`\`\``
         guildRoleCreateEvent.embeds[0].author = {
-          name: displayUsername(perp),
+          name: displayUser(perp),
           icon_url: perp.avatarURL
         }
         await send(guildRoleCreateEvent)
