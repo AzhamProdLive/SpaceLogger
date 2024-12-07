9f2477c2708fd3f2f4cd8ab2f10e4db31513189f
Azham Makes Trash <73955243+AzhamProdLive@users.noreply.github.com>
2024-12-07 21:50:00 +0100

    Update logbots.js

--- src/bot/commands/logbots.js
+++ src/bot/commands/logbots.js
@@ -1,5 +1,5 @@
 const { toggleLogBots } = require('../../db/interfaces/postgres/update')
-const { displayUsername } = require('../utils/constants')
+const { displayUser } = require('../utils/constants')
 
 module.exports = {
   func: async message => {
@@ -11,10 +11,10 @@ module.exports = {
         timestamp: new Date(),
         footer: {
           icon_url: global.bot.user.avatarURL,
-          text: displayUsername(global.bot.user)
+          text: displayUser(global.bot.user)
         },
         author: {
-          name: displayUsername(message.author),
+          name: displayUser(message.author),
           icon_url: message.author.avatarURL
         },
         fields: []
